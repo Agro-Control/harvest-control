88ded8ca2398faeff74f3b36fbc8c9ebec80cc19
Maicon Lara <maiconluan.lara@yahoo.com.br>
2024-03-09 21:48:01 -0300

    docs: fixing phosphor icons import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,7 +51,7 @@ const config = {
                 },
             },
             dropShadow: {
-                side: "0 2px 2px rgba(0, 0, 0, 0.2)",
+                side: "0 2px 2px rgba(0, 0, 0, 0.3)",
             },
             keyframes: {
                 "accordion-down": {
