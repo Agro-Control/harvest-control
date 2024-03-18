c12d812e68c39915c967aafc6d1727fa89611da3
maiconlara <maiconluan.lara@yahoo.com.br>
2024-03-18 15:11:34 -0300

    refactor: adding responsive in panel cards grid

---
+++ src/app/control/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Dashboard() {
+    return (
+        <div className="flex h-screen w-full flex-col items-center justify-start overflow-hidden px-6 pt-10 text-green-950 gap-10 ">
+          
+
+            
+        </div>
+    );
+}
--- src/components/control/fast-access-card.tsx
+++ src/components/control/fast-access-card.tsx
@@ -10,7 +10,7 @@ const FastAccessCard = ({title, image}: FastAcessCardProps) => {
 
     return (
         <div
-            className={`${bgImage} flex min-h-[96px] w-full cursor-pointer flex-col items-center justify-center rounded-xl bg-center bg-no-repeat transition-opacity hover:opacity-80`}
+            className={`${bgImage} ${image === "dashboard" ? "col-span-2 lg:col-span-1" : ""} flex min-h-[192px] w-full cursor-pointer flex-col items-center justify-center rounded-xl bg-center bg-no-repeat transition-opacity hover:opacity-80`}
         >
             <p className="font-poppins text-2xl font-semibold text-white">{title}</p>
         </div>
