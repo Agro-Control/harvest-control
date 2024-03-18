e29bc6439e031f606b5e9d64af2027a7794842aa
maiconlara <maiconluan.lara@yahoo.com.br>
2024-03-18 13:51:07 -0300

    feat: adding active route in side bar

--- src/components/nav/nav-button.tsx
+++ src/components/nav/nav-button.tsx
@@ -1,17 +1,26 @@
 // "use client";
 
 import {Icon} from "@phosphor-icons/react";
+import {usePathname} from "next/navigation";
 
 interface NavButtonProps {
     title: string;
     Icon: Icon;
+    path: string;
 }
 
-const NavButton = ({Icon, title}: NavButtonProps) => {
+const NavButton = ({Icon, title, path}: NavButtonProps) => {
+    const pathname = usePathname();
+    const isActive = pathname === path;
+
+    const activeStyle = isActive
+        ? "border-l-[5px] flex h-[46px] w-full cursor-pointer flex-row items-center justify-start gap-3 rounded-md rounded-r-xl border-green-600 px-6 transition-colors pointer-events-none bg-divider select-none"
+        : "flex h-[46px] w-full cursor-pointer flex-row items-center justify-start gap-3 rounded-xl px-6 transition-colors  hover:bg-divider  select-none ";
+
     return (
-        <div className="flex h-[46px] w-full cursor-pointer flex-row items-center justify-start gap-3 rounded-xl px-6 transition-colors  hover:bg-divider ">
+        <div className={`${activeStyle}`}>
             <Icon className="h-6 w-6" weight="fill" color="#052e14" />
-            <p className="font-jakarta text-md font-medium text-green-950">{title}</p>
+            <p className="text-md font-jakarta font-medium  text-green-950 ">{title}</p>
         </div>
     );
 };
--- src/components/nav/sidebar.tsx
+++ src/components/nav/sidebar.tsx
@@ -19,23 +19,23 @@ const Sidebar = () => {
         <div className="fixed left-0 top-0 flex h-screen w-[308px] flex-col justify-start gap-12 bg-sidebar py-6 drop-shadow-side">
             <SidebarHeader />
             <div className="flex w-full flex-col gap-3 px-2  ">
-                <NavButton Icon={WindowsLogo} title="Painel de controle" />
-                <NavButton Icon={ChartPieSlice} title="Dashboard" />
-                <NavButton Icon={FileText} title="Ordens de serviço" />
-                <NavButton Icon={FilePdf} title="Relatórios" />
-                <NavButton Icon={UsersThree} title="Gestão de pessoas" />
-                <NavButton Icon={Truck} title="Máquinas" />
-                <NavButton Icon={MapPin} title="Talhões" />
-                <NavButton Icon={Buildings} title="Gestão de empresas" />
+                <NavButton Icon={WindowsLogo} title="Painel de controle" path="/control" />
+                <NavButton Icon={ChartPieSlice} title="Dashboard" path="/control/dashboard" />
+                <NavButton Icon={FileText} title="Ordens de serviço" path="/control/orders" />
+                <NavButton Icon={FilePdf} title="Relatórios" path="/control/report" />
+                <NavButton Icon={UsersThree} title="Gestão de pessoas" path="/control/users" />
+                <NavButton Icon={Truck} title="Máquinas" path="/control/machine" />
+                <NavButton Icon={MapPin} title="Talhões" path="/control/field" />
+                <NavButton Icon={Buildings} title="Gestão de empresas" path="/control/companies" />
             </div>
             <div className="mt-auto flex h-[14vh] w-full flex-col justify-end gap-3 self-end bg-transparent px-2">
                 <div className="group flex h-[40px] w-full cursor-pointer flex-row items-center justify-start gap-3 rounded-xl px-6 transition-colors  hover:bg-divider ">
                     <GearSix className="h-6 w-6 text-green-950 " weight="fill" />
-                    <p className="text-md font-jakarta font-medium text-green-950   ">Configuração</p>
+                    <p className="text-md font-jakarta font-medium text-green-950">Configuração</p>
                 </div>
                 <div className="group flex h-[40px] w-full cursor-pointer flex-row items-center justify-start gap-3 rounded-xl px-6 transition-colors  hover:bg-divider ">
                     <SignOut className="h-6 w-6 text-green-950  " weight="fill" />
-                    <p className="text-md font-jakarta font-medium text-green-950 ">Sair</p>
+                    <p className="text-md font-jakarta font-medium text-green-950">Sair</p>
                 </div>
             </div>
         </div>
