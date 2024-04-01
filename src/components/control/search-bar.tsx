d62fc40e0793c5b1cfdeda48249a40f5d0d78acb
maiconlara <maiconluan.lara@yahoo.com.br>
2024-04-01 10:29:59 -0300

    refactor: adding search url param

--- src/components/control/search-bar.tsx
+++ src/components/control/search-bar.tsx
@@ -1,16 +1,27 @@
 "use client";
-
+import {useState} from "react";
 import {Input} from "@/components/ui/search";
+import {useQueryState} from "nuqs";
+import useDebounce from "@/utils/hooks/useDebounce";
 
 interface SearchBarProps {
     text: string;
 }
 
 const SearchBar = ({text}: SearchBarProps) => {
-    return (
-       
-        <Input placeholder={text} spellCheck="false" />
-    ); 
+    const setQueryParam = useQueryState("query")[1];
+    const [query, setQuery] = useState("");
+    const searchLengthRule = query.length > 0 && query.length < 3;
+    useDebounce(
+        () => {
+            if (searchLengthRule) return;
+            setQueryParam(query);
+        },
+        1500,
+        [query],
+    );
+
+    return <Input onChange={(e) => setQuery(e.target.value)} placeholder={text} spellCheck="false" />;
 };
 
 export default SearchBar;
