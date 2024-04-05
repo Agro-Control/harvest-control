d5e5bdd35d9add9354ceec709be825ea06cb7885
maiconlara <maiconluan.lara@yahoo.com.br>
2024-04-05 15:58:02 -0300

    feat: adding view user information modal

---
+++ src/components/control/view-user-modal.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {Form, FormControl, FormField, FormItem, FormMessage} from "@/components/ui/form";
+import {PasswordInput} from "@/components/ui/password-input";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {ReactNode} from "react";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+
+interface EditUserProps {
+    userInformation: {
+        matricula: string;
+        name: string;
+        status: string;
+        profile: string;
+        contract: string;
+    };
+    children: ReactNode;
+}
+
+const ViewUserModal = ({children, userInformation}: EditUserProps) => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>{children}</DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle className="font-poppins text-green-950">Usuário</DialogTitle>
+                    <DialogDescription>Você está visualizando as informações do usuário.</DialogDescription>
+                </DialogHeader>
+
+                <div className="grid grid-cols-2 gap-4 py-4">
+                    <Input disabled className=" col-span-1" id="name" placeholder="Nome" value={userInformation.name} />
+
+                    <Input disabled className="col-span-1 " id="role" placeholder="Cargo" value={userInformation.name} />
+
+                    <Input disabled className="col-span-2 " id="email" placeholder="Email" />
+
+                    <Input disabled className="col-span-1 " id="identifier" placeholder="CPF" />
+
+                    <Input disabled className="col-span-1 " id="phone" placeholder="Telefone" />
+
+                    <Input disabled className="col-span-1 " id="address" placeholder="CEP" />
+
+                    <Input disabled className="col-span-1 " id="state" placeholder="Estado" />
+
+                    <Input disabled className="col-span-1 " id="city" placeholder="Cidade" />
+
+                    <Input disabled className="col-span-1 " id="neighborhood" placeholder="Bairro" />
+
+                    <Input disabled className="col-span-1 " id="street" placeholder="Rua" />
+
+                    <Input disabled className="col-span-1 " id="number" placeholder="Número" />
+
+                    <Input disabled className="col-span-1 " id="company" placeholder="Empresa" />
+                </div>
+
+                <DialogFooter>
+                    <Button
+                        type="submit"
+                        form="user-form"
+                        className="font-regular rounded-xl bg-green-500 py-5 font-poppins text-green-950 ring-0 transition-colors hover:bg-green-600"
+                    >
+                        Confirmar
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+};
+export default ViewUserModal;
