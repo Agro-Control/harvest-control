9424c47008d8f1bf830bfdb090decb1adffb9d03
maiconlara <maiconluan.lara@yahoo.com.br>
2024-04-08 17:07:27 -0300

    refactor: changing form message size

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -155,7 +155,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn("text-[0.8rem] font-medium text-red-500 dark:text-red-900", className)}
+      className={cn("text-[0.7rem] font-medium text-red-500 dark:text-red-900", className)}
       {...props}
     >
       {body}
