6bcd9461ce56a009aa904b5413d20f6318aae895
maiconlara <maiconluan.lara@yahoo.com.br>
2024-04-08 17:08:10 -0300

    refactor: adding navigation in fast access cards

--- src/components/control/fast-access-card.tsx
+++ src/components/control/fast-access-card.tsx
@@ -1,4 +1,6 @@
-type CardType = "machine" | "operator" | "dashboard";
+import Link from "next/link";
+
+type CardType = "machines" | "users" | "dashboard";
 
 interface FastAcessCardProps {
     title: string;
@@ -6,14 +8,17 @@ interface FastAcessCardProps {
 }
 
 const FastAccessCard = ({title, image}: FastAcessCardProps) => {
-    const bgImage = image === "machine" ? "bg-machine" : image === "operator" ? "bg-operator" : "bg-dashboard";
+    const bgImage = image === "machines" ? "bg-machine" : image === "users" ? "bg-operator" : "bg-dashboard";
 
     return (
+        <Link href={`control/${image}`}>
         <div
             className={`${bgImage} ${image === "dashboard" ? "col-span-2 lg:col-span-1" : ""} flex min-h-[192px] w-full cursor-pointer flex-col items-center justify-center rounded-xl bg-center bg-no-repeat transition-opacity hover:opacity-80`}
-        >
+            
+            >
             <p className="font-poppins text-2xl font-semibold text-white">{title}</p>
         </div>
+           </Link>
     );
 };
 export default FastAccessCard;
