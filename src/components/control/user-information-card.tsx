3139b5172a47b4a7d0133acf2d1b3932aa82a2da
maiconlara <maiconluan.lara@yahoo.com.br>
2024-05-02 13:37:56 -0300

    refactor: removing mocked user and adding user from context, adding translation

--- src/components/control/user-information-card.tsx
+++ src/components/control/user-information-card.tsx
@@ -1,17 +1,23 @@
 "use client";
-import {User, EnvelopeSimple, House, AddressBook, Pencil} from "@phosphor-icons/react";
+import {User as UserIcon, EnvelopeSimple, House, AddressBook, Pencil, Phone} from "@phosphor-icons/react";
 import EditProfileModal from "./edit-profile-modal";
 import logoRounded from "@/assets/logo-rounded.svg";
-import {mockuser} from "@/app/control/page";
+import { Skeleton } from "@/components/ui/skeleton"
+import { useTranslation } from "react-i18next";
+import User from "@/types/user";
 
 
 interface UserInformationCardProps {
-    user: mockuser;
+    user: User | null; 
 }
 
 const UserInformationCard = ({user}: UserInformationCardProps) => {
+    const {t} = useTranslation();
+    const userData =  user ? user.usuario : null;
+    const formattedPhone = userData ? userData.telefone.replace(/(\d{2})(\d{4,5})(\d{4})/, "($1) $2-$3") : '';
+
     return (
-        <div className="flex w-full flex-col items-start justify-start gap-4">
+        <div className="flex w-full flex-col items-start justify-start gap-4 min-h-[115px]">
             <div className="flex w-full flex-row items-center justify-start gap-2">
                 <img src={logoRounded.src} className="h-[24px] w-[24px]" />
                     <p className="font-medium">Informações do usuário</p>
@@ -23,23 +29,21 @@ const UserInformationCard = ({user}: UserInformationCardProps) => {
 
             <div className="mt-4 grid w-full grid-cols-5 gap-6 font-medium">
                 <div className="flex w-full flex-row gap-2">
-                    <User className="h-6 w-6" weight="fill" color="#052e14" />
-                    <p>{user.name}</p>
+                    <UserIcon className="h-6 w-6" weight="fill" color="#052e14" />
+                   {userData ? <p>{userData.nome}</p> : <Skeleton className="h-6 w-full" /> } 
                 </div>
                 <div className="flex w-full flex-row gap-2">
                     <AddressBook className="h-6 w-6" weight="fill" color="#052e14" />
-                    <p>{user.tipo}</p>
+                    {userData ? <p>{t(userData.tipo)}</p> : <Skeleton className="h-6 w-full" />}
                 </div>
 
                 <div className="flex w-full flex-row gap-2">
-                    <House className="h-6 w-6" weight="fill" color="#052e14" />
-                    <p>
-                        {user.cidade}-{user.estado}
-                    </p>
+                    <Phone className="h-6 w-6" weight="fill" color="#052e14" />
+                    {userData ? <p>{formattedPhone}</p> : <Skeleton className="h-6 w-full" />}
                 </div>
                 <div className="col-span-2 flex w-full flex-row gap-2">
                     <EnvelopeSimple className="h-6 w-6" weight="fill" color="#052e14" />
-                    <p>{user.email}</p>
+                    {userData ? <p>{userData.email}</p> : <Skeleton className="h-6 w-2/3" />}
                 </div>
             </div>
             <div className="flex h-full w-full flex-row"></div>
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,19 +1,21 @@
+export interface UserData {
+    id: number;
+    nome: string;
+    email: string;
+    cpf: string;
+    telefone: string;
+    status: string;
+    data_contratação: string;
+    gestor_id: string;
+    empresa_id: string;
+    matricula: string | null;
+    turno: string | null;
+    tipo: string;
+}
+
 interface User {
-    usuario: {
-        id: number;
-        nome: string;
-        email: string;
-        cpf: string;
-        telefone: string;
-        status: string;
-        data_contratação: string;
-        gestor_id: string;
-        empresa_id: string;
-        matricula: string | null;
-        turno: string | null;
-        tipo: string;
-    },
-    token: string
+    usuario: UserData;
+    token: string;
 }
 
 export default User;
