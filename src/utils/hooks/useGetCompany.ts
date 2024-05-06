2bdd09b8bd069ad480a44aa8a70933f14832959d
maiconlara <maiconluan.lara@yahoo.com.br>
2024-05-06 12:42:24 -0300

    refactor: changing name of use get company

---
+++ src/utils/hooks/useGetCompany.ts
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import { api } from "@/lib/api";
+
+const getCompanyRequest = async (id: number | null) => {
+    const { data } = await api.get(`/empresas/${id}`);
+    return data;
+};
+
+export const useGetCompany = (id: number | null) => {
+    return useQuery({
+        queryKey: ["company"],
+        enabled: false,
+        queryFn: () => getCompanyRequest(id),
+    });
+};
