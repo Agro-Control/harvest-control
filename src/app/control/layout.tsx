f38dba6b6625abbe2150d9865f221b5509722c88
maiconlara <maiconluan.lara@yahoo.com.br>
2024-05-08 10:50:04 -0300

    refactor: adding a loading state in layout - see if there's other way to do it

--- src/app/control/layout.tsx
+++ src/app/control/layout.tsx
@@ -1,16 +1,23 @@
+"use client";
+
 import Sidebar from "@/components/nav/sidebar";
+import {useAuth} from "@/utils/hooks/useAuth";
+import Loading from "../loading";
 
 export default function ControlLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    return (
+    const {user, isLoading} = useAuth();
+    const isUserNull = user === null;
+    const isLoadingUser = isLoading || isUserNull;
+    return isLoadingUser ? (
+        <Loading />
+    ) : (
         <div className="flex w-full flex-row items-center">
             <Sidebar />
-            <div className="flex h-full ml-[308px] w-full flex-col overflow-y-auto overflow-x-hidden">
-                {children}
-            </div>
+            <div className="ml-[308px] flex h-full w-full flex-col overflow-y-auto overflow-x-hidden">{children}</div>
         </div>
     );
 }
