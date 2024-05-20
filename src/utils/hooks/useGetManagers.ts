a313a342cb4290371d71412dcea3c6adf553c616
maiconlara <maiconluan.lara@yahoo.com.br>
2024-05-20 16:19:05 -0300

    refactor: removing tipo and adding unidade_id in useGetManager

--- src/utils/hooks/useGetManagers.ts
+++ src/utils/hooks/useGetManagers.ts
@@ -5,17 +5,17 @@ import GetGestor from "@/types/get-gestor";
 const getManagersRequest = async (
     grupo_id: number | null,
     empresa_id: number | null,
+    unidade_id: number | null,
     codigo: string | null,
     status: string | null,
-    tipo: string | null,
 ) => {
     const {data} = await api.get<GetGestor>("/gestores", {
         params: {
             grupo_id,
             codigo,
             status,
-            tipo,
             empresa_id,
+            unidade_id,
         },
     });
     return data;
@@ -24,17 +24,17 @@ const getManagersRequest = async (
 export const useGetManagers = (
     grupo_id: number | null,
     empresa_id: number | null,
+    unidade_id: number | null,
     status: string | null,
     codigo: string | null,
-    tipo: string | null,
 ) => {
     return useQuery({
         queryKey: ["managers"],
-        queryFn: () => getManagersRequest(grupo_id, empresa_id, codigo, status, tipo),
+        queryFn: () => getManagersRequest(grupo_id, empresa_id, unidade_id, codigo, status),
         enabled: !!grupo_id,
         retry: (failureCount, error) => {
             if (error instanceof Error && error.message.includes("404")) {
-                if (failureCount == 3) return false;
+                if (failureCount == 2) return false;
             }
 
             return true;
