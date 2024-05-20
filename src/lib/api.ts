303cb89df6a3e2dd9c0cb9a1de527055c08e896c
maiconlara <maiconluan.lara@yahoo.com.br>
2024-05-20 10:33:28 -0300

    feat: adding Auth header interceptor

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,9 @@
+import useCookie from "@/utils/hooks/useCookies";
 import axios from "axios";
+
 const apiUrl = "http://localhost:5000/";
 
+const { getCookie } = useCookie();
 
 export const api = axios.create({
   baseURL: apiUrl,
@@ -8,5 +11,15 @@ export const api = axios.create({
     "Content-Type": "application/json",
     Accept: "application/json",
     "Access-Control-Allow-Origin": "*",
-},
+  },
 });
+
+api.interceptors.request.use((config) => {
+  const token = getCookie("user_session");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
