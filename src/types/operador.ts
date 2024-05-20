78f098e5efdfc7da373a664fb8ed333ad78ca292
maiconlara <maiconluan.lara@yahoo.com.br>
2024-05-20 15:26:14 -0300

    refactor: changing operator type

--- src/types/operador.ts
+++ src/types/operador.ts
@@ -1,16 +1,18 @@
 interface Operador {
-    id?: number | null;
-    nome?: string | null;
-    email?: string | null;
-    cpf?: string | null;
-    telefone?: string | null;
-    status?: string | null;
-    data_contratacao?: string | null;
-    gestor_id?: number | null;
-    empresa_id?: number | null;
-    matricula?: string | null;
-    turno?: string | null;
-    tipo?: string | null;
+    id: number;
+    nome: string;
+    email: string | null;
+    cpf: string;
+    telefone: string | null;
+    status: string;
+    data_contratacao: string;
+    gestor_id: string;
+    unidade_id: string;
+    empresa_id: number | null;
+    matricula: string | null;
+    turno: string;
+    tipo: string;
+    grupo_id: number | null;
 }
 
-export default Operador;
+export default Operador; 
