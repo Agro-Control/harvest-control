59272e28d504a0eaccf878ae395b0f1c8d20cf77
Maicon Lara <maiconluan.lara@yahoo.com.br>
2024-05-27 00:02:43 -0300

    feat: adding active operators hook and card to dashboard

--- src/components/control/dashboard/active-users-card.tsx
+++ src/components/control/dashboard/active-users-card.tsx
@@ -1,95 +1,29 @@
-
 "use client";
+import {useGetOperatorsByGroup} from "@/utils/hooks/useGetOperatorsByGroup";
 import {User, Users, Pulse} from "@phosphor-icons/react";
-
+import {useAuth} from "@/utils/hooks/useAuth";
+import DoubleDataCard from "./double-data-card";
 
 const ActiveUsersCard = () => {
+    const {user} = useAuth();
+    const grupo_id = user && user?.grupo_id;
 
+    const {data, isLoading, isRefetching, refetch} = useGetOperatorsByGroup(grupo_id);
+    const isLoadingData = isLoading || isRefetching;
 
     return (
-        <div className="col-span-2 lg:col-span-1 flex h-full w-full flex-col items-start justify-between gap-6 rounded-2xl border border-divider bg-white p-4 lg:max-h-[130px]">
-        <div className="flex w-full flex-row gap-4">
-            <div className="flex  h-9 w-9  items-center justify-center overflow-hidden rounded-lg
-            
-            bg-green-400/50
-            border
-            border-green-500
-            ">
-                <User className="h-5 w-5 text-green-950 " />
-            </div>
-
-            <div className="flex  flex-row md:flex-col">
-                <p
-                    className="
-                    line-clamp-1
-                    text-base
-                    font-bold
-                    leading-5
-                    text-green-950
-
-                "
-                >
-                    Relação de usuarios
-                </p>
-                <p
-                    className="
-                        line-clamp-1
-                        text-xs
-                        font-normal
-                        leading-4
-                    text-gray-500
-
-                    "
-                >
-                    Totais e Ativos
-                </p>
-            </div>
-        </div>
-        <div className="flex w-full flex-col gap-2 lg:gap-0 lg:flex-row items-center justify-between">
-            <div
-                className="flex h-9 w-full
-        flex-row
-        items-center
-        justify-center
-        gap-3
-        rounded-full
-        bg-gray-500/10
-        border border-divider   
-        px-3
-        text-base
-        font-normal
-        text-black
-        xl:w-auto
-"
-            >
-                <Users className="h-5 w-5 text-green-950 " />
-                <p> Usuarios totais: 123</p>
-            </div>
-
-            <div className="flex flex-1 h-8 max-w-[1px] bg-divider"/>
-        
-            <div
-                className="flex h-9 w-full
-        flex-row
-        items-center
-        justify-center
-        gap-3
-        rounded-full
-        bg-gray-500/10
-        border border-divider 
-        px-3
-        text-base
-        font-normal
-        text-black
-        xl:w-auto
-"
-            >
-                <Pulse className="h-5 w-5 text-green-900 " />
-                Usuarios ativos: 78
-            </div>
-        </div>
-    </div>
-    )
-}
+        <DoubleDataCard
+            title="Relação de usuários"
+            subtitle="Totais e Ativos"
+            Icon={User}
+            isLoading={isLoadingData}
+            firstDataTitle="Usuarios totais: "
+            firstDataValue={data?.operadores_totais || 0}
+            FirstDataIcon={Users}
+            secondDataTitle="Usuarios ativos: "
+            secondDataValue={data?.operadores_ativos || 0}
+            SecondDataIcon={Pulse}
+        />
+    );
+};
 export default ActiveUsersCard;
-
---
+++ src/utils/hooks/useGetOperatorsByGroup.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { api } from "@/lib/api";
+
+
+
+interface OperatorsByGroup {
+    operadores_ativos: number;
+    operadores_totais: number;
+}
+
+const getOperatorsByGroup = async (grupo_id: number | null) => {
+    const { data } = await api.get<OperatorsByGroup>("dashboards/operadores_operando", {
+        params: {
+            grupo_id
+        }
+    });
+    return data;
+};
+
+export const useGetOperatorsByGroup = (grupo_id: number | null) => {
+    return useQuery({
+        queryKey: ["operators_by_group", grupo_id],
+        enabled: true, 
+        queryFn: () => getOperatorsByGroup(grupo_id),
+    });
+};
