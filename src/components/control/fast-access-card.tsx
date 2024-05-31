9b41a344f4435fe348c6cf894fe0122885ca96e8
maiconlara <maiconluan.lara@yahoo.com.br>
2024-05-31 14:30:45 -0300

    refactor: removing group page

--- src/components/control/fast-access-card.tsx
+++ src/components/control/fast-access-card.tsx
@@ -1,6 +1,6 @@
-type CardType = "machines" | "companies" | "units" | "users" | "dashboard" | "group";
+type CardType = "machines" | "companies" | "units" | "users" | "dashboard";
+import {usePathname} from "next/navigation";
 import Link from "next/link";
-
 
 interface FastAcessCardProps {
     title: string;
@@ -13,17 +13,19 @@ const bgImageMap = {
     "companies": "bg-company",
     "units": "bg-unity",
     "users": "bg-operator",
-    "group": "bg-group",
     "dashboard": "bg-dashboard"
 };
 
 const FastAccessCard = ({title, image, isLast = false}: FastAcessCardProps) => {
+
+    const pathname = usePathname();
+
     const bgImage = bgImageMap[image];
-    const isCompaniesPath = image === "companies" || image === "units" || image === "group";
-    const path = isCompaniesPath ? "business-management/" : "control/";
+    const isCompaniesPath = image === "companies" || image === "units";
+    const path = isCompaniesPath ? (pathname === "/control" ? "control/business-management/" : "business-management/") : "control/";
 
     return (
-        <Link href={`${path}${image}`}  className={` ${isLast ? "md:col-span-2 lg:col-span-1" : ""} flex min-h-[192px] w-full`} >
+        <Link href={`${path}${image}`}  className={` ${isLast ? "md:col-span-2 lg:col-span-1" : " lg:col-span-1"} flex min-h-[192px] w-full`} >
         <div
             className={`${bgImage} flex min-h-[192px] w-full cursor-pointer flex-col items-center text-center justify-center rounded-xl bg-center bg-no-repeat transition-opacity hover:opacity-80`}
             
