b9714516dbd6db089e98c11ad47b44ef76757301
cristopherkovalski <cristopherkovalski@gmail.com>
2024-05-30 22:44:03 -0300

    feat: filter by type for report page, new function to format duration and translate for reports

---
+++ src/utils/functions/formatDuration.ts
@@ -0,0 +1,17 @@
+export default function formatDuration(seconds: number): string {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secs = seconds % 60;
+
+    const hoursDisplay = hours > 0 ? `${hours}h` : "";
+    const minutesDisplay = minutes > 0 ? `${minutes}m` : "";
+    const secondsDisplay = secs > 0 ? `${secs}s` : "";
+
+    if (hours > 0) {
+        return `${hoursDisplay} ${minutesDisplay} ${secondsDisplay}`.trim();
+    } else if (minutes > 0) {
+        return `${minutesDisplay} ${secondsDisplay}`.trim();
+    } else {
+        return `${secondsDisplay}`;
+    }
+}
