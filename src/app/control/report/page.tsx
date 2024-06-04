3c1d60108d6a4ebe485f2c2b5dfdf0cb13bf38ba
maiconlara <maiconluan.lara@yahoo.com.br>
2024-06-04 16:38:29 -0300

    refactor: changing report page and adding translations

--- src/app/control/report/page.tsx
+++ src/app/control/report/page.tsx
@@ -1,56 +1,98 @@
 "use client";
-import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {Table, TableBody, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 import StatusCodeHandler from "@/components/status-code-handler";
-import LoadingAnimation from "@/components/loading-animation";
-import { useAuth } from "@/utils/hooks/useAuth";
-import { useQueryState } from "nuqs";
-import { AxiosError } from "axios";
-import { useEffect, useState } from "react";
-import { useGetEvents } from "@/utils/hooks/useGetEvents";
-import Evento from "@/types/evento";
-import ReportRow from "@/components/control/report/report-row";
 import ReportCard from "@/components/control/report/report-card";
-import Filter from "@/components/control/filter";
-import FilterInformationLabel from "@/types/filter-information-label";
-import FilterWithLabel from "@/components/control/filter-with-label";
-import Operador from "@/types/operador";
+import OrderEvent from "@/components/control/report/order-event";
+import ReportRow from "@/components/control/report/report-row";
+import LoadingAnimation from "@/components/loading-animation";
+import {useGetEvents} from "@/utils/hooks/useGetEvents";
+import {useAuth} from "@/utils/hooks/useAuth";
+import {useQueryState} from "nuqs";
+import Evento from "@/types/evento";
+import {AxiosError} from "axios";
+import {useEffect} from "react";
+import {Funnel, ClockCountdown, TrendUp, Play, HourglassMedium, Truck, Gear, Pause, Footprints, Repeat} from "@phosphor-icons/react";
 
 export default function Reports() {
     const auth = useAuth();
     const user = auth.user;
 
-    const evetnTypeFilter: FilterInformationLabel = {
-        filterItem: [
-            { value: "all", label: "Todos" },
-            { value: "operacao", label: "Operação" },
-            { value: "transbordo", label: "Transbordo" },
-            { value: "deslocamento", label: "Deslocamento" },
-            { value: "aguardando transbordo", label: "Aguardando Transbordo" },
-            { value: "manutencao", label: "Manutenção" },
-            { value: "clima", label: "Clima" },
-            { value: "troca de turno", label: "Troca de Turno" },
-        ],
-    };
-
     // Hooks para obter os parâmetros da URL e os eventos da API
     const [query] = useQueryState("query");
     const [tipoEvento] = useQueryState("Eventos");
-    const { data: { eventos = [] } = {}, error, isError, isLoading, refetch, isRefetching } = useGetEvents(parseInt(query!));
+    const {
+        data: {eventos = []} = {},
+        error,
+        isError,
+        isLoading,
+        refetch,
+        isRefetching,
+    } = useGetEvents(parseInt(query!));
     const isLoadingData = isLoading || isRefetching;
 
-    useEffect(() => {
-       
-    }, [eventos, tipoEvento]);
+    useEffect(() => {}, [eventos, tipoEvento]);
 
     return (
-        <div className="flex h-screen w-full flex-col items-center justify-start gap-10 px-6 pt-10 text-green-950 ">
-            <div className="flex w-full flex-row justify-between items-start ">
+        <div className="flex min-h-screen w-full flex-col items-center justify-start gap-10 px-6 pt-10 text-green-950 ">
+            <div className="flex w-full flex-row items-start justify-between ">
                 <p className="font-poppins text-4xl font-medium">Relatório de Eventos</p>
-                <ReportCard />
             </div>
-            <form className="fflex w-full flex-row items-start justify-start gap-4">
-                <FilterWithLabel filter={evetnTypeFilter} paramType="Eventos" />
-            </form>
+
+
+<div className="flex flex-col xl:flex-row w-full gap-2">
+            <div className="flex w-full flex-col items-start justify-between gap-6 rounded-2xl border border-divider bg-white p-4 lg:col-span-1 lg:min-h-[130px]">
+                <div className="flex w-full flex-row gap-4">
+                    <div className="flex  h-9 w-9  items-center justify-center overflow-hidden rounded-lg border border-green-500 bg-green-400/50">
+                        <ClockCountdown className="h-5 w-5 text-green-950 " />
+                    </div>
+
+                    <div className="flex flex-col gap-1 md:gap-0 ">
+                        <p className="line-clamp-1 text-base font-bold leading-5 text-green-950">Eventos</p>
+                        <p className="line-clamp-3 text-xs font-normal text-gray-500">
+                            Todos os eventos do sistema e a contagem de suas ocorrências
+                        </p>
+                    </div>
+                </div>
+
+                <div className="grid w-full grid-cols-1 gap-2 md:grid-cols-4 ">
+                    <OrderEvent Icon={TrendUp} event={0} isLoading={false} title="total" />
+                    <OrderEvent Icon={Play} event={0} isLoading={false} title="inicio_ordem_servico" />
+                    <OrderEvent Icon={HourglassMedium} event={0} isLoading={false} title="duracao_total" />
+                    <OrderEvent Icon={Footprints} event={0} isLoading={false} title="deslocamento" />
+                    <OrderEvent Icon={Gear} event={0} isLoading={false} title="manutencao" />
+                    <OrderEvent Icon={Truck} event={0} isLoading={false} title="transbordo" />
+                    <OrderEvent Icon={Repeat} event={0} isLoading={false} title="operacao" />
+                    <OrderEvent Icon={Pause} event={0} isLoading={false} title="aguardando_transbordo" />
+                </div>
+            </div>
+
+            <div className="flex  md:min-w-[300px] flex-col items-start justify-between gap-6 rounded-2xl border border-divider bg-white p-4 lg:col-span-1 lg:min-h-[130px]">
+            <div className="flex w-full flex-row gap-4">
+                    <div className="flex  h-9 w-9  items-center justify-center overflow-hidden rounded-lg border border-green-500 bg-green-400/50">
+                        <Funnel className="h-5 w-5 text-green-950 " />
+                    </div>
+
+                    <div className="flex flex-col gap-1 md:gap-0 ">
+                        <p className="line-clamp-1 text-base font-bold leading-5 text-green-950">Filtros</p>
+                        <p className="line-clamp-1 text-xs font-normal text-gray-500">
+                            Ordem de serviço
+                        </p>
+                    </div>
+                </div>
+
+                <ReportCard />
+               
+               
+            </div>
+
+
+</div>
+
+
+
+
+
+
             <Table>
                 <TableHeader>
                     <TableRow>
@@ -64,24 +106,31 @@ export default function Reports() {
 
                 {/* Renderizar os eventos filtrados na tabela */}
                 <TableBody>
-                {!isError && !isLoadingData && eventos && eventos.length > 0 &&
-                        eventos.filter(evento => {
-                            if (tipoEvento) {
-                                const nomeMatch = evento.nome && evento.nome.toLowerCase().includes(tipoEvento.toLowerCase());
-                                return nomeMatch;
-                            } else {
-                                return true; // Se nenhum tipo de evento estiver selecionado, exibir todos os eventos
-                            }
-                        }).map((evento: Evento) => (
-                            <ReportRow key={evento.id} evento={evento} />
-                        ))
-                    }
+                    {!isError &&
+                        !isLoadingData &&
+                        eventos &&
+                        eventos.length > 0 &&
+                        eventos
+                            .filter((evento) => {
+                                if (tipoEvento) {
+                                    const nomeMatch =
+                                        evento.nome && evento.nome.toLowerCase().includes(tipoEvento.toLowerCase());
+                                    return nomeMatch;
+                                } else {
+                                    return true; // Se nenhum tipo de evento estiver selecionado, exibir todos os eventos
+                                }
+                            })
+                            .map((evento: Evento) => <ReportRow key={evento.id} evento={evento} />)}
                 </TableBody>
             </Table>
             {/* Renderizar animação de carregamento, se necessário */}
             {isLoadingData && <LoadingAnimation />}
             {/* Renderizar mensagem se não houver eventos */}
-            {eventos.length === 0 && !isLoadingData && <div className="flex w-full items-center justify-center font-medium">Nenhum evento encontrado.</div>}
+            {eventos.length === 0 && !isLoadingData && !isError && (
+                <div className="flex w-full items-center justify-center font-medium">
+                    Pesquise uma ordem para mostrar os eventos
+                </div>
+            )}
             {/* Renderizar mensagem de erro, se houver */}
             {isError && !isLoadingData && <StatusCodeHandler requisitionType="report" error={error as AxiosError} />}
         </div>
