6407fedef9c3d734dfc5cc7930c039a8ce3d2fb2
maiconlara <maiconluan.lara@yahoo.com.br>
2024-06-04 16:39:11 -0300

    refactor: re styling report car

--- src/components/control/report/report-card.tsx
+++ src/components/control/report/report-card.tsx
@@ -1,18 +1,26 @@
 import SearchBar from "@/components/control/search-bar";
-
+import FilterInformationLabel from "@/types/filter-information-label";
+import FilterWithLabel from "@/components/control/filter-with-label";
 
 const ReportCard = () => {
+    const eventTypeFilter: FilterInformationLabel = {
+        filterItem: [
+            {value: "all", label: "Todos"},
+            {value: "operacao", label: "Operação"},
+            {value: "transbordo", label: "Transbordo"},
+            {value: "deslocamento", label: "Deslocamento"},
+            {value: "aguardando transbordo", label: "Aguardando Transbordo"},
+            {value: "manutencao", label: "Manutenção"},
+            {value: "clima", label: "Clima"},
+            {value: "troca de turno", label: "Troca de Turno"},
+        ],
+    };
 
     return (
-        <div
-            className={`bg-report lg:col-span-1 flex min-h-[120px] w-[500px] px-10 flex-row items-center text-center justify-start rounded-xl bg-center bg-no-repeat`}
-            >
-                <div className="flex flex-col w-full items-start justify-start gap-2" >
-                    <p className="text-white text-sm">Busque pela Ordem de Serviço</p>
-                    <div className="flex w-[50%]">
-                    <SearchBar isReport text="Digite a ordem..." />
-                         </div>
-                    </div>
+        <div className="flex w-full flex-col items-center gap-2">
+           
+                <SearchBar isReport text="Identificador" />
+                <FilterWithLabel isFull filter={eventTypeFilter} paramType="Eventos" />
         </div>
     );
 };
