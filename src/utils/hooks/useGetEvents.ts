fe777a4ea187d9287a79b34fb79f591470e97806
Maicon Lara <maiconluan.lara@yahoo.com.br>
2024-06-09 18:35:16 -0300

    refactor: adding event type in get event

--- src/utils/hooks/useGetEvents.ts
+++ src/utils/hooks/useGetEvents.ts
@@ -3,15 +3,19 @@ import { api } from "@/lib/api";
 
 import GetEventos from "@/types/get-eventos";
 
-const getEventsRequest = async (id_ordem: number | null) => {
-    const { data } = await api.get<GetEventos>(`/eventos/${id_ordem}`)
+const getEventsRequest = async (id_ordem: number | null, nome: string | null) => {
+    const { data } = await api.get<GetEventos>(`/eventos/${id_ordem}`,{
+        params: {
+            nome
+        }
+    })
     return data;
 };
 
-export const useGetEvents = (id_ordem: number | null) => {
+export const useGetEvents = (id_ordem: number | null, nome: string | null) => {
     return useQuery({
         queryKey: ["events", id_ordem],
-        queryFn: () => getEventsRequest(id_ordem),
+        queryFn: () => getEventsRequest(id_ordem, nome),
         retry: (failureCount, error) => {
 
             if (error instanceof Error && error.message.includes("404")) {
