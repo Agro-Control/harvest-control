1c24261812a6a9ae5fb4a1afe9b743b26fe78a30
maiconlara <maiconluan.lara@yahoo.com.br>
2024-06-10 14:31:13 -0300

    feat: starting maintenance page

---
+++ src/components/control/dashboard/maintenance-card.tsx
@@ -0,0 +1,16 @@
+
+
+const data = {
+    "evento": "manutencao",
+    "qtd_eventos_mes": 3,
+    "tempo_total_manutencao": 213,
+    "eventos_manutencao_dia_atual": 3,
+    "qtd_maquinas_manutencao_dia": 1
+  }
+
+
+
+const MaintenanceCard = () => {
+
+}
+export default MaintenanceCard;
