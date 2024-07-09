4eb07e494a12a12d5c8ea2fdf878fcbcb23e830f
maiconlara <maiconluan.lara@yahoo.com.br>
2024-07-08 23:49:34 -0300

    refactor: changes in view machine modal

--- src/components/control/machine/view-machine-modal.tsx
+++ src/components/control/machine/view-machine-modal.tsx
@@ -8,17 +8,23 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
-import {ReactNode, useEffect, useState} from "react";
-import {Button} from "@/components/ui/button";
-import {Input} from "@/components/ui/input";
-import Maquina from "@/types/maquina";
-import {useGetUnit} from "@/utils/hooks/useGetUnit";
-import {Calendar, Factory, GasPump, GearSix, Tag, Truck, Wrench} from "@phosphor-icons/react";
-import useFormattedDate from "@/utils/formatDate";
-
+import {Calendar, Factory, GasPump, GearSix, Tag, Truck, Wrench, Files} from "@phosphor-icons/react";
+import OperationalTimeItem from "@/components/control/dashboard/operational-time-item";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import {useGetOperationalTime} from "@/utils/hooks/useGetOperationalTime";
+import {useGetMachineInfo} from "@/utils/hooks/useGetMachineInfo";
+import {ReactNode, useEffect, useState} from "react";
+import {useGetUnit} from "@/utils/hooks/useGetUnit";
+import useFormattedDate from "@/utils/formatDate";
+import tractor from "@/assets/tractorGreen.svg";
+import colhedora from "@/assets/colhedora.png";
+import {Button} from "@/components/ui/button";
+import {useAuth} from "@/utils/hooks/useAuth";
+import {useTranslation} from "react-i18next";
+import {Input} from "@/components/ui/input";
 import AdvancedInfo from "./advanced-info";
-import { useGetMachineInfo } from "@/utils/hooks/useGetMachineInfo";
+import Maquina from "@/types/maquina";
+import Image from "next/image";
 
 interface EditMachineProps {
     machine: Maquina;
@@ -26,9 +32,49 @@ interface EditMachineProps {
 }
 
 const ViewMachineModal = ({children, machine}: EditMachineProps) => {
+    const {t} = useTranslation();
+
     const [open, setOpen] = useState(false);
     const {data: unidade, error, isError, isLoading, refetch, isRefetching} = useGetUnit(machine.unidade_id!);
-    const {data, isLoading: isLoadingInfo, refetch: refetchInfo, isRefetching: isRefetchingInfo} = useGetMachineInfo(machine.id);
+    const auth = useAuth();
+    const user = auth.user;
+    const isAdmin = user?.tipo === "D";
+    const empresa_id = user && user?.empresa_id;
+    const grupo_id = user && user?.grupo_id;
+
+    const {data: operationalTime, refetch: refetchOperationalTime} = useGetOperationalTime(
+        grupo_id,
+        isAdmin ? null : empresa_id,
+        machine.id,
+    );
+
+    console.log(operationalTime);
+
+    const {
+        data,
+        isLoading: isLoadingInfo,
+        refetch: refetchInfo,
+        isRefetching: isRefetchingInfo,
+    } = useGetMachineInfo(machine.id);
+
+    const formatDuration = (duration: number | null | undefined): string => {
+
+        if(duration === undefined) return "";
+
+        if (duration === 0 || duration === null) {
+            return "Em andamento";
+        }
+
+        const hours = Math.floor(duration / 3600);
+        const minutes = Math.floor((duration % 3600) / 60);
+        const seconds = duration % 60;
+
+        const hoursStr = hours > 0 ? `${hours}hr` : "";
+        const minutesStr = minutes > 0 ? `${minutes}min  ` : "";
+        const secondsStr = seconds > 0 ? ` ${seconds}s` : "";
+
+        return `${hoursStr} ${minutesStr}  ${secondsStr}`.trim();
+    };
 
     const formatDate = useFormattedDate();
 
@@ -37,7 +83,6 @@ const ViewMachineModal = ({children, machine}: EditMachineProps) => {
             refetch();
             refetchInfo();
         }
-
     }, [open]);
 
     const handleClose = () => {
@@ -48,94 +93,133 @@ const ViewMachineModal = ({children, machine}: EditMachineProps) => {
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>{children}</DialogTrigger>
             <Tabs defaultValue="account">
-                <DialogContent className=" sm:max-w-[480px]">
+                <DialogContent className=" sm:min-w-[1120px]">
                     <DialogHeader className="gap-2">
                         <div>
-                            <DialogTitle className="font-poppins text-green-950">Dados da Máquina</DialogTitle>
-                            <DialogDescription className="m">
-                                Você está visualizando as informações da Máquina.
-                            </DialogDescription>
+                            <DialogTitle className="font-poppins text-green-950">
+                                <div className="flex flex-row items-center justify-start gap-3">
+                                    <div className="flex  h-9 w-9  items-center justify-center overflow-hidden rounded-lg border border-green-500 bg-green-400/50 ">
+                                        <Image className="h-5 w-5 " src={tractor} alt="T" />
+                                    </div>
+                                    <p>{`${machine.nome || ""} - ${machine.modelo || ""}`}</p>
+                                </div>
+                            </DialogTitle>
                         </div>
 
-                        <TabsList className="grid w-full grid-cols-2 ">
+                        <TabsList className="grid max-w-[240px] grid-cols-2 ">
                             <TabsTrigger value="basic">Básico</TabsTrigger>
                             <TabsTrigger value="advanced">Avançado</TabsTrigger>
                         </TabsList>
                     </DialogHeader>
                     <TabsContent value="basic">
-                        <div className="grid grid-cols-2 gap-4 py-4">
-                            <Input
-                                disabled
-                                Icon={Truck}
-                                className=" col-span-2"
-                                id="nome"
-                                placeholder="Codigo"
-                                value={machine.nome! || "Não informado"}
-                            />
-                            <Input
-                                disabled
-                                Icon={GearSix}
-                                className="col-span-2 "
-                                id="modelo"
-                                placeholder="Modelo"
-                                value={machine.modelo! || "Não informado"}
-                            />
-                            <Input
-                                disabled
-                                Icon={Wrench}
-                                className="col-span-2 "
-                                id="fabricante"
-                                placeholder="Fabricante"
-                                value={machine.fabricante || "Não informado"}
-                            />
-                            {false && <Input
-                                disabled
-                                Icon={GasPump}
-                                className="col-span-1 "
-                                id="capacidade_opercional"
-                                placeholder="Capacidade Operacional"
-                                value={machine.capacidade_operacional || "Não informado"}
-                            />}
-                            <Input
-                                disabled
-                                Icon={Calendar}
-                                className="col-span-1 "
-                                id="data_aquisicao"
-                                placeholder="Data Aquisição"
-                                value={formatDate(machine.data_aquisicao!) || "Não informado"}
-                            />
-                            {!isLoading && (
-                                <Input
-                                    disabled
-                                    Icon={Factory}
-                                    className="col-span-1 "
-                                    id="unidade"
-                                    placeholder="Unidade"
-                                    value={!isLoading && unidade?.nome}
+                        <div className="flex h-full w-full flex-row items-start justify-start gap-4">
+                            <div className="flex min-w-[340px] flex-col gap-4">
+                                <Image
+                                    className="h-[242px] w-[335px] rounded-xl object-cover"
+                                    quality={100}
+                                    placeholder="blur"
+                                    priority={true}
+                                    src={colhedora}
+                                    alt="T"
                                 />
-                            )}
-                            <Input
-                                disabled
-                                Icon={Tag}
-                                className="col-span-1 "
-                                id="status"
-                                placeholder="Status"
-                                value={machine.status === "A" ? "Ativo" : "Inativo"}
-                            />
-                        </div>
+                                <div className="flex h-full max-h-[1px] min-h-[1px] w-full flex-1 bg-divider" />
 
-                        <DialogFooter>
-                            <Button
-                                onClick={handleClose}
-                                type="submit"
-                                className="font-regular rounded-xl bg-green-500 py-5 font-poppins text-green-950 ring-0 transition-colors hover:bg-green-600"
-                            >
-                                Voltar
-                            </Button>
-                        </DialogFooter>
+                                <div className="flex w-full flex-col gap-3">
+                                    <div className="flex flex-row items-center justify-start gap-3">
+                                        <div className="flex  h-9 w-9  items-center justify-center overflow-hidden rounded-lg border  border-divider bg-gray-500/10 ">
+                                            <Files className="h-5 w-5 " />
+                                        </div>
+                                        <p className="text-sm font-bold">Informações</p>
+                                    </div>
+                                    <div className="flex w-full flex-col gap-2">
+                                        <div className="flex w-full flex-row gap-2">
+                                            <p className="text-sm font-bold">Nome: </p>
+                                            <p className="text-sm font-semibold text-green-800">{machine.nome || ""}</p>
+                                        </div>
+                                        <div className="flex w-full flex-row gap-2">
+                                            <p className="text-sm font-bold">Modelo: </p>
+                                            <p className="text-sm font-semibold text-green-800">
+                                                {machine.modelo || ""}
+                                            </p>
+                                        </div>
+                                        <div className="flex w-full flex-row gap-2">
+                                            <p className="text-sm font-bold">Fabricante: </p>
+                                            <p className="text-sm font-semibold text-green-800">
+                                                {machine.fabricante || ""}
+                                            </p>
+                                        </div>
+                                        <div className="flex w-full flex-row gap-2">
+                                            <p className="text-sm font-bold">Unidade: </p>
+                                            <p className="text-sm font-semibold text-green-800">
+                                                {unidade?.nome || ""}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col w-full h-full gap-3"> 
+                            <div className="flex w-full flex-row items-center justify-between rounded-2xl border border-divider bg-white p-4 ">
+                               
+                               <div className="flex w-full flex-row gap-4">
+                               
+                               <div className="flex  h-9 w-9  items-center justify-center overflow-hidden rounded-lg border border-green-500 bg-green-400/50 " >
+                                   <Files className="h-5 w-5 text-green-950 " />
+                               </div>
+                               
+                               <div className="flex  flex-row md:flex-col">
+                                <div className="flex flex-row gap-2"> 
+                                   <p className="line-clamp-1 text-xs font-normal leading-4 text-gray-500  ">
+                                       Ultimo evento
+                                   </p>
+                                   <p className="line-clamp-1 text-xs font-normal leading-4 text-gray-500  ">
+                                      {formatDate(data?.ultimos_eventos[0].data_inicio!)}
+                                   </p>
+                                </div>
+                                   <p className="line-clamp-1 text-sm font-bold leading-5 text-green-950 ">
+                                       {t(data?.ultimos_eventos[0].nome!) || ""}
+                                   </p>
+                               </div>
+
+                               </div> 
+                                    {/* TODO: verificar o tamanho do texto */}
+                                  <p className="w-auto text-sm text-center font-bold text-green-950 px-4 rounded-full bg-green-400/50"> 
+                                   {formatDuration(data?.ultimos_eventos[0].duracao) || ""}
+                                  </p>
+
+                            </div>  
+
+
+                            <div className="col-span-2 flex h-full w-full flex-col items-start justify-between gap-6 rounded-2xl border border-divider bg-white p-4  lg:min-h-[130px] xl:col-span-1">
+                                <div className="text-black flex h-full w-full flex-col items-center justify-between gap-3 rounded-2xl border border-divider bg-gray-500/10 px-3 py-12 text-base font-normal">
+                                <OperationalTimeItem
+                                    title="Operação"
+                                    description="Tempo total de eventos produtivos."
+                                    color="#16a34a"
+                                    value={operationalTime?.operacionais || 0}
+                                    total={operationalTime?.tempo_jornada_total || 100}
+                                />
+                                <OperationalTimeItem
+                                    title="Improdutivo"
+                                    description="Tempo total de eventos improdutivos."
+                                    color="#FF5F15"
+                                    value={operationalTime?.improdutivos || 0}
+                                    total={operationalTime?.tempo_jornada_total || 100}
+                                />
+                                <OperationalTimeItem
+                                    title="Manutenção"
+                                    description="Tempo total de eventos manutenção."
+                                    color="#ef4c51"
+                                    value={operationalTime?.manutencao || 0}
+                                    total={operationalTime?.tempo_jornada_total || 100}
+                                />
+                                </div>
+                            </div>  
+                           
+                        </div>
+                        </div>
                     </TabsContent>
                     <TabsContent value="advanced">
-                       {data && <AdvancedInfo data={data} />}
+                        {/* {data && <AdvancedInfo data={data} />} */}
                         <DialogFooter>
                             <Button
                                 onClick={handleClose}
@@ -152,3 +236,4 @@ const ViewMachineModal = ({children, machine}: EditMachineProps) => {
     );
 };
 export default ViewMachineModal;
+
